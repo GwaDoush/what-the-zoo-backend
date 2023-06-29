8351327f185e3ff89f434dcb744d6eb5df33fef4
Guillaume Morel <Guillaume>
2023-06-29 23:26:17 +0200

    Initial commit

---
+++ src/main/kotlin/com/gmorel/whatthezoobackend/WhatTheZooBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.gmorel.whatthezoobackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhatTheZooBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhatTheZooBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/gmorel/whatthezoobackend/WhatTheZooBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gmorel.whatthezoobackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WhatTheZooBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
